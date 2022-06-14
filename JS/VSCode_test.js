c4a9fd8da4b50a80b9500e7539c659babb77d209
Илья Рябов <dr.weirdo89@gmail.com>
2022-06-14 15:46:55 +0300

    add VSCode-test.js

---
+++ JS/VSCode_test.js
@@ -0,0 +1,37 @@
+// let user_name = "Ilya"
+// let name2 = "Ilya"
+
+// console.log(name2)
+
+
+// let age = 32
+// console.log(age)
+
+// let person = user_name + " " + age
+// console.log(user_name + "    " + age)
+
+//  let user_age = 30
+//  let work_period = 10
+
+//  let total_time = user_age / work_period
+//  console.log(" total_time =", total_time)
+
+// let morning = false
+// console.log("morning = ", morning, typeof(morning))
+
+// let evening = true
+// console.log("evening = ", evening, typeof(evening))
+
+let apples = 15
+let bananas = 5
+
+let compare = apples >= bananas
+console.log("compare =", compare)
+
+if(compare){
+    console.log("Compare is", true)
+} else if (apples == 15) {
+    console.log("apples == ", apples)
+} else {
+    console.log("!!_Compare is", false)
+}
